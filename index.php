12cecb7ac61d1414ebe083b00e7b7bc84559d8e0
fourteamsss0 <hiendhcn3b@gmail.om>
2019-11-16 09:52:46 +0700

    commit

--- index.php
+++
@@ -1,6 +0,0 @@
-<!-- đầu trang -->
-<?php require_once __DIR__."/public/fontend/header.php"?>
-<!-- nội dung -->
-<?php require_once __DIR__."/public/fontend/content_main_index.php";?>
-<!-- chân trang -->
-<?php require_once __DIR__."/public/fontend/footer.php";?>
