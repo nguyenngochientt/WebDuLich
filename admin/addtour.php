24c8c51ebf36a9739d24269f34aeeab615d35a16
NguyenNgocHien <gravitonnguyen@gmail.com>
2019-11-27 04:53:11 +0700

    upload images tour

--- admin/addtour.php
+++ admin/addtour.php
@@ -101,44 +101,21 @@
                                             <input type="text" class="form-control" placeholder="Giá vé trẻ em...">
                                         </div>
                                     </div>
+                                    <!-- up load -->
 
-                                    <div class="ln_solid"></div>
-                                    <div class="form-group">
-                                        <div class="col-md-9 col-sm-9  offset-md-3">
-                                            <button type="button" class="btn btn-primary">Cancel</button>
-                                            <button type="reset" class="btn btn-primary">Reset</button>
-                                            <button type="submit" class="btn btn-success">Submit</button>
-                                        </div>
-                                    </div>
-                                    <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Dropzone multiple file uploader</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <p>Drag multiple files to the box below for multi upload or click to select files. This is for demonstration purposes only, the files are not uploaded to any server.</p>
-                    <form action="form_upload.html" class="dropzone"></form>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                  </div>
-                </div>
+                                    <!-- end up load -->
+
+
                                 </form>
                             </form>
+                            <?php include __DIR__."/ul.php" ?>
+                            <div class="form-group">
+                                <div class="col-md-9 col-sm-9  offset-md-3">
+                                    <button type="button" class="btn btn-primary">Cancel</button>
+                                    <button type="reset" class="btn btn-primary">Reset</button>
+                                    <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- admin/layout/header.php
+++ admin/layout/header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title>Gentelella Alela! | </title>
+    <title>Travel Book | </title>
 
     <!-- Bootstrap -->
     <link href="layout/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
