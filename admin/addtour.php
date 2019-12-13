e205aaf400156801e2fe7a4035c9a67f3f302f74
Nguyễn Ngọc Hiền <gravitonnguyen@gmail.com>
2019-12-13 09:01:58 +0700

    commit upload images

--- admin/addtour.php
+++ admin/addtour.php
@@ -118,16 +118,7 @@
                             </form>
                             <div class="box-upload">
                             <h2>Upload hình ảnh</h2>
-                            <form action="upload.php" method="POST" enctype="multipart/form-data" id="formUpload" onsubmit="return false;">
-                                <div class="progress">
-                                    <div class="progress-bar">0%</div>
-                                </div>
-                                <input type="file" name="img_file[]" multiple="true" onchange="previewImg(event);" id="img_file" accept="image/*">
-                                <div class="box-preview-img"></div>
-                                <button type="reset" class="btn-reset">Làm mới</button>
-                                <button type="submit" class="btn-submit">Upload</button>
-                                <div class="output"></div>
-                            </form>
+                        <?php  include __DIR__."/upload.php"?>
                             </div>
                             <script src="admin/js/jquery.js"></script>
                             <script src="admin/js/jquery.form.js"></script>
--- admin/upload.php
+++ admin/upload.php
@@ -1,12 +1,79 @@
 <?php
+// Ấn định  dung lượng file ảnh upload
+define ("MAX_SIZE","8000");
 
-// Sử dụng vòng lặp for để lưu từng file trong mảng
-foreach($_FILES['img_file']['name'] as $name => $value)
+// hàm này đọc phần mở rộng của file. Nó được dùng để kiểm tra nếu
+// file này có phải là file hình hay không .
+function getExtension($str) {
+$i = strrpos($str,".");
+if (!$i) { return ""; }
+$l = strlen($str) - $i;
+$ext = substr($str,$i+1,$l);
+return $ext;
+}
+
+//This variable is used as a flag. The value is initialized with 0 (meaning no
+// error  found)
+//and it will be changed to 1 if an errro occures.
+//If the error occures the file will not be uploaded.
+$errors=0;
+//checks if the form has been submitted
+if(isset($_POST['Submit']))
 {
-	$name_img = stripslashes($_FILES['img_file']['name'][$name]);
-	$source_img = $_FILES['img_file']['tmp_name'][$name];
-	$path_img = "res/".$name_img;
-	move_uploaded_file($source_img, $path_img);
+// lấy tên file upload
+$image=$_FILES['image']['name'];
+// Nếu nó không rỗng
+if ($image)
+{
+// Lấy tên gốc của file
+$filename = stripslashes($_FILES['image']['name']);
+//Lấy phần mở rộng của file
+$extension = getExtension($filename);
+$extension = strtolower($extension);
+// Nếu nó không phải là file hình thì sẽ thông báo lỗi
+if (($extension != "jpg") && ($extension != "jpeg") && ($extension !=
+"png") && ($extension != "gif"))
+{
+// xuất lỗi ra màn hình
+echo '<h1>Đây không phải là file hình!</h1>';
+$errors=1;
+}
+else
+{
+//Lấy dung lượng của file upload
+$size=filesize($_FILES['image']['tmp_name']);
+if ($size > MAX_SIZE*1024)
+{
+echo '<h1>Vượt quá dung lượng cho phép!</h1>';
+$errors=1;
+}
+
+// đặt tên mới cho file hình up lên
+$image_name=time().'.'.$extension;
+// gán thêm cho file này đường dẫn
+$newname="images/".$image_name;
+// kiểm tra xem file hình này đã upload lên trước đó chưa
+$copied = copy($_FILES['image']['tmp_name'], $newname);
+if (!$copied)
+{
+echo '<h1> File hình này đã tồn tại </h1>';
+$errors=1;
+}}}}
+
+if(isset($_POST['Submit']) && !$errors)
+{
+echo "<h1>File hình đã được Upload thành công </h1>";
 }
 
 ?>
+
+<!-- nhớ đặt enctype to "multipart/frm-data"
+và sử dụng  input type "file" -->
+<form name="newad" method="post" enctype="multipart/form-data"
+action="">
+<table>
+<tr><td><input type="file" name="image"></td></tr>
+<tr><td><input name="Submit" type="submit" value="Upload image">
+</td></tr>
+</table>
+</form>
