5f284c9f8d5cc14685c818d7af08906d631f3529
NguyenNgocHien <gravitonnguyen@gmail.com>
2019-12-30 23:22:17 +0700

    commit

--- admin/function/guider/guider.php
+++ admin/function/guider/guider.php
@@ -1,39 +1,39 @@
 <?php 
    
     // ///////////////////////////////////////
-    include 'tour-server.php';
+    include 'guider-server.php';
     //use TourObject\Tour;
   //  include 'model/connectDB.php';
     use model\connectDB;      
     
-    class TXSTour{
+    class TXSGuider{
         private $connectDB="";
-        public $tour="";
+        public $guider="";
         public function __construct(){
-            $this->tour=new ArrayObject();
+            $this->guider=new ArrayObject();
             $this->connectDB=new connectDB("tctdlich");
             $this->connectDB->connect();
         }
         public function HienThi(){
-            $select="select * from tour";
+            $select="select * from guider";
             $i=0;
             $result=mysqli_query( $this->connectDB->conn, $select);
             if(mysqli_num_rows($result)>0){
                 
                 while($row=mysqli_fetch_assoc($result)){
                     $row=(object) $row;
-                   $this->tour[$i]=$row;
+                   $this->guider[$i]=$row;
                    $i++;
                   
                 }
             }
             
-            return $this->tour;
+            return $this->guider;
         }
-        public function Them($tour){
-            $tour;
-            $sql="insert into tour(name_tour,tour_guide_id,id_region,place_start,place_des,date_go,date_back,img_url,num_adults,num_child,price_adult,price_child,id_category)
-                values('".$tour->name_tour."','".$tour->tour_guide_id."','".$tour->id_region."','".$tour->place_start."','".$tour->place_des."','".$tour->date_go."','".$tour->date_back."','".$tour->img_url."','".$tour->num_adult."','".$tour->num_child."','".$tour->price_adults."','".$tour->price_child."','".$tour->id_category."')";
+        public function Them($guider){
+            $guider;
+            $sql="insert into tour(id_guider,name_guider,birthday,address,tel,img_url)
+                values('$guider->id_gudier','$guider->name_guider','$guider->birthday','guider->$guider->tel','$guider->img_url')";
             //$result=mysqli_query( $connectDB->conn, $select);
 
             if (mysqli_query($this->connectDB->conn, $sql)) {
@@ -43,24 +43,16 @@
                 echo "Error: " . $sql . "<br>" . mysqli_error($this->connectDB->conn);
             }
         }
-        public function Sua($tour){
+        public function Sua($guider){
             $sql="
             update from tour
-            set name_tour='$tour->name_tour';
+            set name_guider='$guider->name_guider';
             update from tour
-            set tour_guide_id='$tour->tour_guide_id';
+            set guider->birthday='$guider->birthday';
             update from tour
-            set id_region='$tour->id_region';
+            set guider->tel='$guider->tel';
             update from tour
-            set place_start='$tour->place_start';
-            update from tour
-            set place_des='$tour->place_des';
-            update from tour
-            set date_go='$tour->date_go';
-            update from tour
-            set date_back='$tour->date_back';
-            update from tour
-            set tour_guide_id='$tour->tour_guide_id';
+            set guider->img_url='$guider->img_url';
             ";
         }
     }
