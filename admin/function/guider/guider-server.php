0e5547582adaf9127e704214bf8191ec7fa83c8c
NguyenNgocHien <gravitonnguyen@gmail.com>
2019-12-30 23:14:43 +0700

    commit

--- admin/function/guider/guider-server.php
+++ admin/function/guider/guider-server.php
@@ -2,23 +2,13 @@
    // namespace TourObject;
     class Tour
     {
-        public $id_tour="";
-        public $tour_guide_id="";
-        public $id_region="";
-        public $name_tour="";
-        public $place_start="";
-        public $place_des="";
-        public $date_go="";
-        public $state="";
-        public $date_back="";
+        public $id_guider="";
+        public $name_guider="";
+        public $birthday="";
+        public $address="";
+        public $tel="";
         public $img_url="";
-        public $process_tour="";
-        public $num_child="";
-        public $num_adult="";
-        public $price_adults="";
-        public $price_child="";
-        public $type="";
-        public $id_category="";
+
         public function __construct()
         {
             
