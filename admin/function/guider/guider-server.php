dbd49db27aa324469c7d9c9cd496a24a0c13fa90
NguyenNgocHien <gravitonnguyen@gmail.com>
2019-12-30 23:10:27 +0700

    commit

---
+++ admin/function/guider/guider-server.php
@@ -0,0 +1,29 @@
+<?php 
+   // namespace TourObject;
+    class Tour
+    {
+        public $id_tour="";
+        public $tour_guide_id="";
+        public $id_region="";
+        public $name_tour="";
+        public $place_start="";
+        public $place_des="";
+        public $date_go="";
+        public $state="";
+        public $date_back="";
+        public $img_url="";
+        public $process_tour="";
+        public $num_child="";
+        public $num_adult="";
+        public $price_adults="";
+        public $price_child="";
+        public $type="";
+        public $id_category="";
+        public function __construct()
+        {
+            
+        }
+       
+
+    }
+?>
