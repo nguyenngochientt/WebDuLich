6948da402a96f67d982d6913c4476da2370e8d42
NguyenNgocHien <gravitonnguyen@gmail.com>
2020-01-07 10:05:20 +0700

    commit

--- commont_layout/menu.php
+++ commont_layout/menu.php
@@ -1,8 +1,20 @@
 <div class="container-fluid">
     <div class="row ">
-        <div class="col-sm-12 nav-menu">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="#">TravelBook</a>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8 ">
+            <div class="nav-menu"
+            style="  z-index: 10;
+            position: relative;">
+            <nav class="navbar navbar-expand-lg navbar-dark" 
+            style="text-align: center;
+            margin: 0px auto;
+            display: inline-block;"
+          >
+                <a class="navbar-brand" href="#"
+                style=" font-size: 252%;
+                text-transform: uppercase;
+                font-family: 'Gelasio', serif;"
+                >TravelBook</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -11,13 +23,19 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Trang chủ <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="index.php"
+                            style="font-family: 'Lato', sans-serif;text-transform: uppercase;"    
+                            >Trang chủ </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="gioithieu.php">Giới thiệu</a>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="gioithieu.php"
+                            style="font-family: 'Lato', sans-serif;text-transform: uppercase;"  
+                            >Giới thiệu</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Bài viết</a>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"
+                            style="font-family: 'Lato', sans-serif;text-transform: uppercase;"  
+                            >Bài viết</a>
                         </li>
                         
 
@@ -25,6 +43,9 @@
 
                 </div>
             </nav>
+            </div>
+          
         </div>
+        <div class="col-sm-2"></div>
     </div>
 </div>
